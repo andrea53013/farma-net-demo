89b7e69fd83d3f1015e08dea69949e7af0c1fad8
andrea53013 <101144877+andrea53013@users.noreply.github.com>
2025-10-06 21:57:02 +0200

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 2025-10-06
+- Shtuar testet unitare për API-të
+- Integrim Sentry
+- Implementim CI/CD
---
+++ next.config.js
@@ -0,0 +1,4 @@
+const withPWA = require('next-pwa')({
+  dest: 'public'
+});
+module.exports = withPWA({ /* config */ });
